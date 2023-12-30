08806754d7056d684e904b29bf46f120ce7a373b
Kenneth Fisher <kenandcrys@live.com>
2023-12-30 04:32:58 -0600

    Changed index.js to display the route.

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require('./api/server.js');
 const PORT = process.env.PORT || 9000;
 
 server.listen(PORT, () => {
-    console.log(`Magic happening on ${PORT}`);
+    console.log(`Magic happening on http://localhost:${PORT}`);
 });
