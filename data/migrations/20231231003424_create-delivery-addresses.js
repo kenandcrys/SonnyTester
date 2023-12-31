a6b3c279aedb1e64ade036016207c5d7054d2a7e
Jess Williams <jesswillcode@gmail.com>
2023-12-30 20:59:27 -0500

    finished delivery addresses table

--- data/migrations/20231231003424_create-delivery-addresses.js
+++ data/migrations/20231231003424_create-delivery-addresses.js
@@ -10,9 +10,13 @@ exports.up = function(knex) {
     .references('id')
     .inTable('users')
     .onDelete('CASCADE');
-    table.string('street_address');
-    table.string('city');
-    table.string('postal_code');
+    table.string('street_address', 255).notNullable();
+    table.string('address_line_2').nullable();
+    table.string('address_line_3').nullable();
+    table.string('city', 100).notNullable();
+    table.string('postal_code', 20).notNullable();
+    table.string('contact_number').defaultTo(knex.raw('(SELECT phone FROM users WHERE users.id = user_id)')); //defaulting to users phone number on profile
+    table.string("delivery notes", 128).nullable();
     table.timestamps(true, true);
 };
 
