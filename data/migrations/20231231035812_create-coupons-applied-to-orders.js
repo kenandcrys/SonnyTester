fe76ed796d7ecae85a9d3d5c6e9b62acbb7858b0
Jess Williams <jesswillcode@gmail.com>
2023-12-30 23:54:19 -0500

    completed coupons applied to orders table

--- data/migrations/20231231035812_create-coupons-applied-to-orders.js
+++ data/migrations/20231231035812_create-coupons-applied-to-orders.js
@@ -3,7 +3,33 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('coupons_applied', function(table) {
+    table.increments('id').primary();
+    // Order to which the coupon is applied
+    table
+    .integer('order_id')
+    .unsigned()
+    .references('id')
+    .inTable('orders')
+    .onDelete('CASCADE');
+    // Coupon associated with the order
+    table
+    .integer('coupon_id')
+    .unsigned()
+    .references('id')
+    .inTable('coupons')
+    .onDelete('CASCADE');
+    // User who applied the coupon
+    table
+    .integer('user_id')
+    .unsigned()
+    .references('id')
+    .inTable('users')
+    .onDelete('CASCADE');
+    // Discount amount applied to the order
+    table.decimal('discount_amount', 10, 2).notNullable();
+    table.date('date_applied');
+  });
 };
 
 /**
