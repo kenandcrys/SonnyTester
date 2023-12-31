bdac8227a91df55e197a7500ce1434f7a05059b6
Jess Williams <jesswillcode@gmail.com>
2023-12-31 00:24:08 -0500

    completed cart and cart products tables

--- data/migrations/20231231035602_create-carts.js
+++ data/migrations/20231231035602_create-carts.js
@@ -3,7 +3,24 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('carts', function(table) {
+    table.increments('id').primary();
+    // User who owns the cart
+    table
+    .integer('user_id')
+    .unsigned()
+    .references('id')
+    .inTable('users')
+    .onDelete('CASCADE');
+    // Subtotal of the cart
+    table.decimal('subtotal', 10, 2).defaultTo(0);
+    // Status of the cart (e.g., 'active', 'completed', 'abandoned')
+    table.string('status').notNullable().defaultTo('active');
+    // Session ID for users who are not logged in
+    table.string('session_id').nullable();
+    // Adding default values for the created_at timestamp only
+    table.timestamps(true, true).defaultTo(knex.fn.now());
+  });
 };
 
 /**
