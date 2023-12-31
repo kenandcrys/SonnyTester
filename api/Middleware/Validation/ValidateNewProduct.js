5deba4cbf3291f6f6f1c180595f75add24fe0c60
Kenneth Fisher <kenandcrys@live.com>
2023-12-30 21:09:59 -0600

    Create Product oute kind of

--- api/Middleware/Validation/ValidateNewProduct.js
+++ api/Middleware/Validation/ValidateNewProduct.js
@@ -1,40 +1,49 @@
 
-const ValidateNewProduct = (type, name, description, price) => {
-    
-    if ( !type ) {
-        return {
-            isValid: false, 
-            message: "Please enter a category type."
-        }
-    };
+const ProductModel = require('../../Models/NewProduct/Product'); // Replace with the actual path to your Product model
 
-    if ( !name || typeof name !== 'string' ) {
-        return {
-            isValid: false, 
-            message: "Please enter a valid product name."
-        }
-    };
+const ValidateNewProduct = async (type, name, description, price) => {
+  // Check if a product with the same name already exists
+  const existingProduct = await ProductModel.getByName(name);
 
-    if ( !description || description.lentgh < 10 || description.length > 256) {
-
-        return {
-            isValid: false, 
-            message: "Please enter a valid description with more than 10  and less than 256 characters."
-        }
-    }
-
-    if ( !price || typeof price !== 'number' || price <= 0 ) {
-
-        return {
-            isValid: false, 
-            message: "Please enter a valid price."
-        }
-    }
-
+  if (existingProduct) {
     return {
-        isValid: true,
-        message: 'Product accepted',
+      isValid: false,
+      message: `Product with name "${name}" already exists.`,
     };
+  }
+
+  if (!type) {
+    return {
+      isValid: false,
+      message: 'Please enter a category type.',
+    };
+  }
+
+  if (!name || typeof name !== 'string') {
+    return {
+      isValid: false,
+      message: 'Please enter a valid product name.',
+    };
+  }
+
+  if (!description || description.length < 10 || description.length > 256) {
+    return {
+      isValid: false,
+      message: 'Please enter a valid description with more than 10 and less than 256 characters.',
+    };
+  }
+
+  if (!price || typeof price !== 'number' || price <= 0) {
+    return {
+      isValid: false,
+      message: 'Please enter a valid price.',
+    };
+  }
+
+  return {
+    isValid: true,
+    message: 'Product Created',
+  };
 };
 
 module.exports = ValidateNewProduct;
