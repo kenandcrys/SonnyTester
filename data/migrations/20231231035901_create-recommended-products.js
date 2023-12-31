df8a5317a6b2592838f2ccd0705fc0ec3fbae432
Jess Williams <jesswillcode@gmail.com>
2023-12-30 23:12:59 -0500

    completed recommended products table

--- data/migrations/20231231035901_create-recommended-products.js
+++ data/migrations/20231231035901_create-recommended-products.js
@@ -3,7 +3,25 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('recommendations', function(table) {
+    table.increments('id').primary();
+    // User receiving recommendation
+    table
+    .integer('user_id')
+    .unsigned()
+    .references('id')
+    .inTable('users')
+    .onDelete('CASCADE');
+    // Product being recommended
+    table
+    .integer('product_id')
+    .unsigned()
+    .references('id')
+    .inTable('products')
+    .onDelete('CASCADE');
+    table.unique(['user_id', 'product_id']);
+    table.timestamps(true, false); //timestamp for when recommended to user
+  });
 };
 
 /**
@@ -11,5 +29,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('recommended_products');
 };
