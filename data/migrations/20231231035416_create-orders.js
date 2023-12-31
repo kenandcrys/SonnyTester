f1dd929b32e9162218ed085c66a8e0423e60f412
Jess Williams <jesswillcode@gmail.com>
2023-12-31 00:53:37 -0500

    completed orders and ordered products table

--- data/migrations/20231231035416_create-orders.js
+++ data/migrations/20231231035416_create-orders.js
@@ -3,7 +3,47 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('orders', function(table) {
+    table.increments('id').primary();
+    // User who placed the order
+    table
+      .integer('user_id')
+      .unsigned()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE');
+      // Shipping fee
+    table.decimal('delivery_fee', 10, 2).notNullable();
+    // Total price of the order
+    table.decimal('total_price', 10, 2).notNullable();
+    // Billing address
+    table
+      .integer('billing_address_id')
+      .unsigned()
+      .references('id')
+      .inTable('payment_methods')
+      .onDelete('SET NULL');
+    // Delivery address
+    table
+      .integer('delivery_address_id')
+      .unsigned()
+      .references('id')
+      .inTable('delivery_addresses')
+      .onDelete('SET NULL');
+    // Payment method used for the order
+    table
+      .integer('payment_method_id')
+      .unsigned()
+      .references('id')
+      .inTable('payment_methods')
+      .onDelete('SET NULL');
+    // Order status (e.g., 'pending', 'shipped', 'delivered')
+    table.string('status').notNullable().defaultTo('pending');
+    // Date order has shipped
+    table.date('date_shipped').notNullable();
+    // Adding default values for the created_at timestamp only
+    table.timestamps(true, true).defaultTo(knex.fn.now());
+  });
 };
 
 /**
--- data/migrations/20231231035533_create-ordered-products.js
+++ data/migrations/20231231035533_create-ordered-products.js
@@ -3,7 +3,30 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('ordered_products', function(table) {
+    // Composite primary key
+    table.primary(['order_id', 'product_id']);
+    // Order to which the product belongs
+    table
+      .integer('order_id')
+      .unsigned()
+      .references('id')
+      .inTable('orders')
+      .onDelete('CASCADE');
+    // Product included in the order
+    table
+      .integer('product_id')
+      .unsigned()
+      .references('id')
+      .inTable('products')
+      .onDelete('CASCADE');
+    // Quantity of the product in the order
+    table.integer('quantity').notNullable();
+    // Price of the product at the time of the order
+    table.decimal('price', 10, 2).notNullable();
+    // Adding default values for the created_at timestamp only
+    table.timestamps(true, false).defaultTo(knex.fn.now());
+  });
 };
 
 /**
