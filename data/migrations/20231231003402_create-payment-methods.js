e4e93031fd5836d0e2843e0a2fa93bea530ccf7e
Jess Williams <jesswillcode@gmail.com>
2023-12-30 21:28:02 -0500

    finished payment methods table

--- data/migrations/20231231003402_create-payment-methods.js
+++ data/migrations/20231231003402_create-payment-methods.js
@@ -10,10 +10,21 @@ exports.up = function(knex) {
     .unsigned().references('id')
     .inTable('users')
     .onDelete('CASCADE');
-    table.string('card_type');
-    table.string('last_four_digits');
+    table.string('card_type', 20);
+    //table.string('card_number', 16); this is heavily regulated by PCI DSS, but putting here if we want to encrypt
+    table.string('last_four_digits', 4);
     table.date('expiration_date');
-    // ... other payment method attributes
+    table.string('security_code', 4); //this is heavily regulated by PCI DSS, but putting here if we want to encrypt
+    table.boolean('is_expired').defaultTo(false); 
+    table.boolean('is_disabled').defaultTo(false); 
+    table.boolean('is_default_payment_method').defaultTo(false);
+    table.boolean('is_billing_same_as_delivery_address').defaultTo(false);
+    table.boolean('is_billing_same_as_user_address').defaultTo(false);
+    table.string('billing_address', 255).nullable();
+    table.string('billing_address_line_2').nullable();
+    table.string('billing_address_line_3').nullable();
+    table.string('billing_city', 100).nullable();
+    table.string('billing_postal_code', 20).nullable(); 
     table.timestamps(true, true);
   });
 };
