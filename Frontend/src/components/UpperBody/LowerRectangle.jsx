1ef71dfe8ca5978579a1f9f66d873448c81e6c30
Kenneth Fisher <kenandcrys@live.com>
2024-01-01 07:30:37 -0600

    Bring FE and BE together

---
+++ Frontend/src/components/UpperBody/LowerRectangle.jsx
@@ -0,0 +1,14 @@
+import Button from "./Button";
+function LowerRectangle() {
+  return (
+    <div
+      className="w-[95%] mx-auto h-auto flex gap-[24px] justify-between mt-[20px]"
+    >
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+    </div>
+  );
+}
+
+export default LowerRectangle;
