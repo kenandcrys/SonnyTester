a0032ccd7c49fb79c905b2013aedbead6069c64f
be9n <abdulrahman.nas48@gmail.com>
2024-01-06 21:56:06 +0300

    Added seeders 11-17

---
+++ Backend/data/seeders/15_seed_recommended_products_demo.js
@@ -0,0 +1,40 @@
+const faker = require('faker');
+
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex('recommendations').del()
+    .then(async function () {
+      // Insert seed entries
+      const users = await knex.select('id').from('users');
+      const products = await knex.select('id').from('products');
+
+      const recommendations = [];
+
+      const uniqueUserProductPairs = new Set(); // To track unique user_id and product_id pairs
+
+      outerLoop: for (let i = 0; i < 10; i++) { // Adjust the loop count based on how many entries you want
+        let user, product;
+
+        // Ensure unique user_id and product_id pair
+        do {
+          user = faker.random.arrayElement(users);
+          product = faker.random.arrayElement(products);
+
+          if (i >= users.length * products.length) break outerLoop;
+        } while (uniqueUserProductPairs.has(`${user.id}-${product.id}`));
+
+        uniqueUserProductPairs.add(`${user.id}-${product.id}`);
+
+        const recommendation = {
+          user_id: user.id,
+          product_id: product.id,
+          created_at: faker.date.past(),
+          updated_at: faker.date.recent(),
+        };
+
+        recommendations.push(recommendation);
+      }
+
+      return knex('recommendations').insert(recommendations);
+    });
+};
