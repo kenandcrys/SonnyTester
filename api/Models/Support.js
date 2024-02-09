f069175b286eaaf2bde9dc62010dfc5f207c58bf
yabetsg <yabets.g2@gmail.com>
2024-02-09 17:34:16 -0500

    Convert from function to class based

--- api/Models/Support.js
+++ api/Models/Support.js
@@ -1,32 +1,27 @@
 const { Model } = require("objection");
-
-const Support = () => {
-  function tableName() {
+class Support extends Model {
+  static get tableName() {
     return "supports";
   }
-  function query(...args) {
-    return Model.query(...args).where("role", "support");
-  }
-  function relationMappings() {
-    const User = require("./User");
 
+  static query(...args) {
+    return super.query(...args).where("role", "support");
+  }
+
+  static get relationMappings() {
+    const User = require('./User'); 
     return {
       user: {
         relation: Model.BelongsToOneRelation,
         modelClass: User,
         join: {
-          from: "supports.user_id",
-          to: "users.id",
+          from: 'supports.user_id',
+          to: 'users.id',
         },
       },
+
     };
   }
-  return {
-    tableName,
-    query,
-    relationMappings,
-  };
-};
-
+}
 
 module.exports = Support;
