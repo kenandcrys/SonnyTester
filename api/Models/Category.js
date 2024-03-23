34b9699330d70452f5f479e7f588c7939ce88d41
webdev-michael <webdev.krygore@gmail.com>
2024-03-23 08:04:22 -0700

    Fixed product list seed, started category model

---
+++ api/Models/Category.js
@@ -0,0 +1,13 @@
+// const { Model } = require("objection")
+
+// class Category extends Model {
+//     static get tableName(){
+//         return "category";
+//     }
+
+//     static get realtionMappings() {
+//         return {
+            
+//         }
+//     }
+// }
