5e0cec5494f1331617b307e454deb615a6d9c4ca
Fish <kenandcrys@live.com>
2024-04-05 00:04:33 -0500

    here then not nullable my ass.

--- data/migrations/20231230235937_create-products.js
+++ data/migrations/20231230235937_create-products.js
@@ -2,25 +2,24 @@ exports.up = function (knex) {
   return knex.schema
     .createTable("category", (table) => {
       table.increments('id').primary();
-      table.string("category_name", 255).notNullable();
+      table.string("category_name", 255);
     })
     .createTable("subcategory", (table) => {
       table.increments();
-      table.string("subcategory_name", 255).notNullable();
+      table.string("subcategory_name", 255);
       table
         .integer("categoryId")
-        .notNullable()
         .references("id")
         .inTable("category");
     })
     .createTable("products", (table) => {
       table.increments();
-      table.string("product_name", 255).notNullable();
-      table.string("product_description", 1000).notNullable();
-      table.string("product_price", 255).notNullable();
+      table.string("product_name", 255);
+      table.string("product_description", 1000);
+      table.string("product_price", 255);
       table
         .integer("subcategoryId")
-        .notNullable()
+       
         .references("id")
         .inTable("subcategory");
     });
