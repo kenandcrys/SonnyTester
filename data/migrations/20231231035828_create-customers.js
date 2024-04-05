41e092b329e878e743e3452ece3e1b2165b9213f
Fish <kenandcrys@live.com>
2024-04-05 17:34:51 -0500

    here

--- data/migrations/20231231035828_create-customers.js
+++ data/migrations/20231231035828_create-customers.js
@@ -1,32 +1,23 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.up = function(knex) {
   return knex.schema.createTable('customers', function(table) {
-      // Customer details
-      table.string('user_id').primary()
-          .references('id')
-          .inTable('users')
-          .onDelete('CASCADE');
-      table.integer('number_of_orders').defaultTo(0);
-      table.integer('customer_since_years');
-      table.integer('total_spending');
-      table.integer('average_order_amount');
-      table.date('first_purchase_date');
-      table.date('last_purchase_date');
-      table.integer('seller_id')
-          .unsigned()
-          .references('id')
-          .inTable('users')
-          .onDelete('CASCADE');
+    // Customer details
+    table.string('user_id').primary()
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE');
+    table.integer('number_of_orders').defaultTo(0);
+    table.integer('customer_since_years');
+    table.integer('total_spending');
+    table.integer('average_order_amount');
+    table.date('first_purchase_date');
+    table.date('last_purchase_date');
+    table.string('seller_id') // Use string type here
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE');
   });
 };
 
-/**
-* @param { import("knex").Knex } knex
-* @returns { Promise<void> }
-*/
 exports.down = function(knex) {
   return knex.schema.dropTableIfExists('customers');
 };
