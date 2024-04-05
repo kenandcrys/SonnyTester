d1c290dc788a6a970ab3e758e9cc56c6e62ed9bb
Fish <kenandcrys@live.com>
2024-04-05 17:03:35 -0500

    now this one.

--- data/migrations/20231231035812_create-coupons-applied-to-orders.js
+++ data/migrations/20231231035812_create-coupons-applied-to-orders.js
@@ -3,29 +3,28 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-    return knex.schema.createTable('coupons_applied', function(table) {
+  return knex.schema.createTable('coupons_applied', function(table) {
     table.increments('id').primary();
     // Order to which the coupon is applied
     table
-    .integer('order_id')
-    .unsigned()
-    .references('id')
-    .inTable('orders')
-    .onDelete('CASCADE');
+      .integer('order_id')
+      .unsigned()
+      .references('id')
+      .inTable('orders')
+      .onDelete('CASCADE');
     // Coupon associated with the order
     table
-    .integer('coupon_id')
-    .unsigned()
-    .references('id')
-    .inTable('coupons')
-    .onDelete('CASCADE');
+      .integer('coupon_id')
+      .unsigned()
+      .references('id')
+      .inTable('coupons')
+      .onDelete('CASCADE');
     // User who applied the coupon
     table
-    .string('user_id')
-    .unsigned()
-    .references('id')
-    .inTable('users')
-    .onDelete('CASCADE');
+      .string('user_id')
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE');
     // Discount amount applied to the order
     table.decimal('discount_amount', 10, 2).notNullable();
     table.date('date_applied');
@@ -37,5 +36,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('applied_coupons');
+  return knex.schema.dropTableIfExists('coupons_applied');
 };
