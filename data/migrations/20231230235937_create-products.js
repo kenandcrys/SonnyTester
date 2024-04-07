56d33d2cdda9c852939263399d6e7a99df084e0a
Kenneth Fisher <31420722+kenandcrys@users.noreply.github.com>
2024-04-06 20:22:14 -0500

    Update 20231230235937_create-products.js

--- data/migrations/20231230235937_create-products.js
+++ data/migrations/20231230235937_create-products.js
@@ -31,7 +31,7 @@ exports.up = function(knex) {
           table.string("product_name", 255).notNullable();
           table.string("product_description", 1000).notNullable();
           table.string("product_price", 255).notNullable();
-          table.string("product_images",128);
+          table.string("product_images",1288);
           table
             .integer("subcategoryId")
             .nullable()
